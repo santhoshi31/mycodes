9a973effeff75e70bb021df07eedf14af642f915
Santhoshi <113975284+santhoshi31@users.noreply.github.com>
2025-02-10 11:33:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim()); // Read number of test cases
+
+        while (t-- > 0) {
+            String line = read.readLine().trim(); // Read the array input
+            String[] numsStr = line.split(" ");   // Split the input string by spaces
+            int[] nums =
+                new int[numsStr.length]; // Convert string array to integer array
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int target = Integer.parseInt(read.readLine().trim()); // Read target sum
+
+            Solution ob = new Solution();
+            boolean ans =
+                ob.hasTripletSum(nums, target); // Call the function and store result
+            System.out.println(ans ? "true" : "false"); // Output the result
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Should return true if there is a triplet with sum equal
+    // to x in arr[], otherwise false
+    public static boolean hasTripletSum(int arr[], int target) {
+        // Your code Here
+        if(target < 3 || arr.length < 3)return false;
+        
+        Set<Integer> s = new HashSet<>();
+        
+        for(int i = 0; i<arr.length; i++){
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[i] + arr[j] >= target){
+                    continue;
+                }
+                if(s.contains(target - (arr[i]+arr[j]))){
+                    return true;
+                }
+            }
+            s.add(arr[i]);
+        }
+        return false;
+    }
+}
