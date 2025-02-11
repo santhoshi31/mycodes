09c06f3cd91bad1c96b9afbeadcec26c1c848219
Santhoshi <113975284+santhoshi31@users.noreply.github.com>
2025-02-11 16:02:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find triplets with zero sum/find-triplets-with-zero-sum.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]); // Parse the input array
+            }
+
+            Solution sol = new Solution();
+            if (sol.findTriplets(arr))
+                System.out.println("true");
+            else
+                System.out.println("false");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+class Solution {
+    // Function to find triplets with zero sum.
+    public boolean findTriplets(int[] arr) {
+        // code here.
+        int start = 0;
+        int end = 0;
+        HashSet <Integer> s = new HashSet();
+        for(int i = 0; i<arr.length; i++){
+            for(int j = i+1; j<arr.length;j++){
+                int sum =-(arr[i]+arr[j]);
+                if(s.contains(sum)){
+                return true;
+                }
+                s.add(arr[j]);
+            }
+            s.clear();
+        }
+        return false;
+    }
+}
